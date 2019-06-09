8ab5d1f02f16bca4c5175cf82f240a146c9c2061
JarvisJ <jarvis.a.james@gmail.com>
2019-06-09 18:48:36 -0400

    test
    testing sandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import * as d3 from "d3";
 
 @Component({
   selector: "app-root",
@@ -6,5 +7,32 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "CodeSandbox";
+  title = "D3 Sandbox";
+  @ViewChild("g") g;
+  ngAfterViewInit() {
+    var d3G = d3.select(this.g.nativeElement);
+    var testData = [1, 2, 3];
+
+    var x = d3
+      .scaleOrdinal()
+      .range([0, 500])
+      .domain(["0", "1", "2"]);
+
+    var y = d3
+      .scaleLinear()
+      .range([500, 0])
+      .domain([0, d3.max(testData, d => d)]);
+
+    var rects = d3G.selectAll("rect");
+
+    rects
+      .data(testData)
+      .enter()
+      .append("rect")
+      .merge(rects)
+      .attr("width", d => d * 20)
+      .attr("height", 20)
+      .style("fill", "blue")
+      .attr("transform", (d, i) => `translate(10,${i * 30})`);
+  }
 }
