21d50e66493d4f16ca9669a530d139da4c365b6c
JarvisJ <jarvis.a.james@gmail.com>
2019-06-11 21:45:09 -0400

    add axis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,11 @@ export class AppComponent {
     var testData = [1, 2, 3];
 
     var x = d3
-      .scaleOrdinal()
+      .scaleBand()
       .range([0, 500])
+      .round(true)
+      .paddingInner(0.1)
+      .paddingOuter(0.1)
       .domain(["0", "1", "2"]);
 
     var y = d3
@@ -23,16 +26,30 @@ export class AppComponent {
       .range([500, 0])
       .domain([0, d3.max(testData, d => d)]);
 
+    var xAxis = d3.axisBottom(x);
+    var yAxis = d3.axisRight(y);
+
     var rects = d3G.selectAll("rect");
+
+    d3G
+      .append("g")
+      .attr("transform", "translate(0,500)")
+      .call(xAxis);
+
+    d3G
+      .append("g")
+      .attr("transform", "translate(500,0)")
+      .call(yAxis);
 
     rects
       .data(testData)
       .enter()
       .append("rect")
       .merge(rects)
-      .attr("width", d => d * 20)
-      .attr("height", 20)
-      .style("fill", "blue")
-      .attr("transform", (d, i) => `translate(10,${i * 30})`);
+      .attr("x", (d, i) => x(i))
+      .attr("y", (d, i) => y(d))
+      .attr("width", x.bandwidth())
+      .attr("height", d => 500 - y(d))
+      .style("fill", "steelblue");
   }
 }
