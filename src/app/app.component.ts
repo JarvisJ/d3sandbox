00bd5a42105563de47d91ff000b48368f3ef0dad
JarvisJ <jarvis.a.james@gmail.com>
2019-06-14 22:10:59 -0400

    start of stacked date chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from "@angular/core";
 import * as d3 from "d3";
+import * as DateFns from "date-fns";
+import * as _ from "lodash";
 
 @Component({
   selector: "app-root",
@@ -60,20 +62,23 @@ export class AppComponent {
     var d3G = d3.select(this.g.nativeElement);
     d3G.selectAll("g").remove();
 
-    var testData = [1, 2, 3];
+    var testData = [
+      { date: new Date(), values: [1, 2, 3] },
+      { date: DateFns.addMonths(new Date(), 2), values: [1, 1, 2] }
+    ];
+
+    var minDate = DateFns.min(..._.map(testData, d => d.date));
+    var maxDate = DateFns.max(..._.map(testData, d => d.date));
 
     var x = d3
-      .scaleBand()
-      .range([0, 500])
-      .round(true)
-      .paddingInner(0.1)
-      .paddingOuter(0.1)
-      .domain(["0", "1", "2"]);
+      .scaleTime()
+      .rangeRound([0, 500])
+      .domain([DateFns.addDays(minDate, -5), DateFns.addDays(maxDate, 5)]);
 
     var y = d3
       .scaleLinear()
       .range([500, 0])
-      .domain([0, d3.max(testData, d => d)]);
+      .domain([0, d3.max(testData, d => _.sum(d.values))]);
 
     var xAxis = d3.axisBottom(x);
     var yAxis = d3.axisRight(y);
@@ -92,10 +97,10 @@ export class AppComponent {
       .selectAll("rect")
       .data(testData)
       .join("rect")
-      .attr("x", (d, i) => x(i + ""))
-      .attr("y", (d, i) => y(d))
-      .attr("width", x.bandwidth())
-      .attr("height", d => 500 - y(d))
+      .attr("x", (d, i) => x(d.date) - 20)
+      .attr("y", (d, i) => y(_.sum(d.values)))
+      .attr("width", 30)
+      .attr("height", d => 500 - y(_.sum(d.values)))
       .style("fill", "green");
   }
 
