127113d5d7fdc4dc632708f28208a3c43e0a9f1c
JarvisJ <jarvis.a.james@gmail.com>
2019-06-17 18:07:50 -0400

    added stacks

--- src/app/charts/stacked.component.ts
+++ src/app/charts/stacked.component.ts
@@ -22,6 +22,10 @@ export class StackedComponent {
   @Input() Margin = 100;
   @Input() Height = 500;
   @Input() Width = 500;
+  @Input() Data = [
+    { date: new Date(), seg1: 1, seg2: 2, seg3: 3 },
+    { date: DateFns.addMonths(new Date(), 2), seg1: 1, seg2: 1, seg3: 2 }
+  ];
 
   ngOnInit() {
     this.drawStackedChart();
@@ -33,13 +37,9 @@ export class StackedComponent {
       .attr("transform", `translate(${this.Margin},${this.Margin})`);
     d3G.selectAll("g").remove();
 
-    var testData = [
-      { date: new Date(), values: [1, 2, 3] },
-      { date: DateFns.addMonths(new Date(), 2), values: [1, 1, 2] }
-    ];
-
-    var minDate = DateFns.min(..._.map(testData, d => d.date));
-    var maxDate = DateFns.max(..._.map(testData, d => d.date));
+    var minDate = DateFns.min(..._.map(this.Data, d => d.date));
+    var maxDate = DateFns.max(..._.map(this.Data, d => d.date));
+    var keys = ["seg1", "seg2", "seg3"];
 
     var x = d3
       .scaleTime()
@@ -49,18 +49,40 @@ export class StackedComponent {
     var y = d3
       .scaleLinear()
       .range([500, 0])
-      .domain([0, d3.max(testData, d => _.sum(d.values))]);
+      .domain([0, d3.max(this.Data, d => _.sumBy(keys, k => d[k]))]);
 
     var xAxis = d3
       .axisBottom(x)
       .tickValues(
-        _(testData)
+        _(this.Data)
           .map(d => d.date)
           .uniq()
           .value()
       )
       .tickFormat(d3.timeFormat("%c"));
     var yAxis = d3.axisRight(y);
+
+    var series = d3.stack().keys(keys)(this.Data as any);
+
+    let color = d3
+      .scaleOrdinal()
+      .domain(series.map(d => d.key))
+      .range(
+        d3
+          .quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), series.length)
+          .reverse()
+      )
+      .unknown("#ccc");
+
+    let color2 = d3
+      .scaleOrdinal()
+      .domain(series.map(d => d.key))
+      .range(
+        d3
+          .quantize(t => d3.interpolateSpectral(t * 0.5 + 0.1), series.length)
+          .reverse()
+      )
+      .unknown("#ccc");
 
     d3G
       .append("g")
@@ -80,14 +102,49 @@ export class StackedComponent {
 
     d3G
       .append("g")
+      .selectAll("g")
+      .data(series)
+      .join("g")
+      .attr("fill", d => color(d.key) as any)
       .selectAll("rect")
-      .data(testData)
+      .data(d => d)
       .join("rect")
-      .attr("x", (d, i) => x(d.date) - 20)
-      .attr("y", (d, i) => y(_.sum(d.values)))
-      .attr("width", 30)
-      .attr("height", d => 500 - y(_.sum(d.values)))
-      .style("fill", "green");
+      .attr("x", (d, i) => x(d.data.date) - 15)
+      .attr("y", d => y(d[1]))
+      .attr("height", d => y(d[0]) - y(d[1]))
+      .attr("width", 30); // x.bandwidth());
+
+    d3G
+      .append("g")
+      .selectAll("g")
+      .data(series)
+      .join("g")
+      .attr("fill", d => color2(d.key) as any)
+      .selectAll("rect")
+      .data(d => d)
+      .join("rect")
+      .attr("x", (d, i) => x(d.data.date) + 15)
+      .attr("y", d => y(d[1] * 0.5))
+      .attr("height", d => y(d[0] * 0.5) - y(d[1] * 0.5))
+      .attr("width", 10);
+
+    // .join("rect")
+    // .attr("x", (d, i) => x(d.date) - 20)
+    // .attr("y", (d, i) => y(_.sum(d.values)))
+    // .attr("width", 30)
+    // .attr("height", d => 500 - y(_.sum(d.values)))
+    // .style("fill", "green");
+
+    // d3G
+    //   .append("g")
+    //   .selectAll("rect")
+    //   .data(this.Data)
+    //   .join("rect")
+    //   .attr("x", (d, i) => x(d.date) + 10)
+    //   .attr("y", (d, i) => y(_.sum(d.values) * 0.5))
+    //   .attr("width", 10)
+    //   .attr("height", d => 500 - y(_.sum(d.values) * 0.5))
+    //   .style("fill", "steelblue");
   }
 
   showChart() {
