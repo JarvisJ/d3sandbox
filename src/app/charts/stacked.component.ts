a1fb18c34252230cd80a4fcac9abf64b10dff2f2
jjarvis <jjarvis@promodel.com>
2020-04-22 18:20:19 -0400

    Update stacked.component.ts
    added smart labels

--- src/app/charts/stacked.component.ts
+++ src/app/charts/stacked.component.ts
@@ -18,14 +18,15 @@ import * as _ from "lodash";
 export class StackedComponent {
   title = "D3 Sandbox";
   tabIndex = 1;
+  series;
 
   @ViewChild("gEle") gElement;
   @Input() Margin = 100;
   @Input() Height = 500;
   @Input() Width = 500;
   @Input() Data = [
-    { date: new Date(), seg1: 1, seg2: 2, seg3: 3 },
-    { date: DateFns.addMonths(new Date(), 2), seg1: 1, seg2: 1, seg3: 2 }
+    { date: new Date(), seg1: 1, seg2: .01, seg3: .02, seg4: 3, seg5: .2, seg6: .1 },
+    { date: DateFns.addMonths(new Date(), 2), seg1: 1, seg2: 1, seg3: 2, seg4: 3, seg5: .2, seg6: .1 }
   ];
 
   ngOnInit() {
@@ -40,7 +41,7 @@ export class StackedComponent {
 
     var minDate = DateFns.min(..._.map(this.Data, d => d.date));
     var maxDate = DateFns.max(..._.map(this.Data, d => d.date));
-    var keys = ["seg1", "seg2", "seg3"];
+    var keys = ["seg1", "seg2", "seg3", "seg4", "seg5", "seg6"];
 
     var x = d3
       .scaleTime()
@@ -50,7 +51,7 @@ export class StackedComponent {
     var y = d3
       .scaleLinear()
       .range([500, 0])
-      .domain([0, d3.max(this.Data, d => _.sumBy(keys, k => d[k]))]);
+      .domain([0, d3.max(this.Data, d => _.sumBy(keys, k => d[k] || 0))]);
 
     var xAxis = d3
       .axisBottom(x)
@@ -63,24 +64,24 @@ export class StackedComponent {
       .tickFormat(d3.timeFormat("%c"));
     var yAxis = d3.axisRight(y);
 
-    var series = d3.stack().keys(keys)(this.Data as any);
+    this.series = d3.stack().keys(keys)(this.Data as any);
 
     let color = d3
       .scaleOrdinal()
-      .domain(series.map(d => d.key))
+      .domain(this.series.map(d => d.key))
       .range(
         d3
-          .quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), series.length)
+          .quantize(t => d3.interpolateRainbow(t * 0.8 + 0.3), this.series.length)
           .reverse()
       )
       .unknown("#ccc");
 
     let color2 = d3
       .scaleOrdinal()
-      .domain(series.map(d => d.key))
+      .domain(this.series.map(d => d.key))
       .range(
         d3
-          .quantize(t => d3.interpolateSpectral(t * 0.5 + 0.1), series.length)
+          .quantize(t => d3.interpolateSpectral(t * 0.5 + 0.1), this.series.length)
           .reverse()
       )
       .unknown("#ccc");
@@ -101,10 +102,12 @@ export class StackedComponent {
       .attr("transform", "translate(500,0)")
       .call(yAxis);
 
+    var barWidth = 30;
+
     d3G
       .append("g")
       .selectAll("g")
-      .data(series)
+      .data(this.series)
       .join("g")
       .attr("fill", d => color(d.key) as any)
       .selectAll("rect")
@@ -113,12 +116,12 @@ export class StackedComponent {
       .attr("x", (d, i) => x(d.data.date) - 15)
       .attr("y", d => y(d[1]))
       .attr("height", d => y(d[0]) - y(d[1]))
-      .attr("width", 30); // x.bandwidth());
+      .attr("width", barWidth); // x.bandwidth());
 
     d3G
       .append("g")
       .selectAll("g")
-      .data(series)
+      .data(this.series)
       .join("g")
       .attr("fill", d => color2(d.key) as any)
       .selectAll("rect")
@@ -128,38 +131,38 @@ export class StackedComponent {
       .attr("y", d => y(d[1] * 0.5))
       .attr("height", d => y(d[0] * 0.5) - y(d[1] * 0.5))
       .attr("width", 10);
-      
 
-      var textEl = d3G
-      .append("g")
+
+
+    // this.smartLabelForceLayout(d3G, x, y, color);
+    var textOffset = 30;
+    var textData = _.flatMap(this.series, d => _.map(d, dd => { return { key: d.key, x: x(dd.data.date) - textOffset - barWidth / 2, y: y(dd[1] - (dd[1] - dd[0]) / 2), value: dd[1] - dd[0] } }));
+
+    var textEl = d3G.append("g")
       .selectAll("g")
-      .data(series)
-      .join("g")
-      .attr("fill", d => color(d.key) as any)
-      .selectAll("text")
-      .data(d => d)
+      .data(textData)
       .join("text")
-      .attr("x", (d, i) => x(d.data.date) - 15-2)
-      .attr("y", d => y(d[1]))
-      .text((d,i)=>d[1]-d[0])
-      .attr("text-anchor","end")  
-      .attr("alignment-baseline","middle");
+      .attr("fill", d => color(d.key) as any)
+      .attr("x", (d, i) => d.x)
+      .attr("y", d => d.y)
+      .text((d, i) => _.round(d.value, 2))
+      .attr("text-anchor", "end")
+      .attr("alignment-baseline", "middle");
 
-      d3G
-      .append("g")
+    var lineEl = d3G.append("g")
       .selectAll("g")
-      .data(textEl)
-      .join("g")
-      .attr("fill", d => color(d.key) as any)
-      .selectAll("text")
-      .data(d => d)
-      .join("text")
-      .attr("x", (d, i) => x(d.data.date) - 15-2)
-      .attr("y", d => y(d[1]))
-      .text((d,i)=>d[1]-d[0])
-      .attr("text-anchor","end")  
-      .attr("alignment-baseline","middle");
+      .data(textData)
+      .join("line")
+      .style("stroke", d => color(d.key) as any)
+      .attr("x1", (d, i) => d.x)
+      .attr("y1", d => d.y)
+      .attr("x2", (d, i) => d.x + textOffset)
+      .attr("y2", d => d.y)
+      .text((d, i) => _.round(d.value, 2))
+      .attr("text-anchor", "end")
+      .attr("alignment-baseline", "middle");
 
+    this.smartLabelForceLayout(textEl, lineEl, textData);
 
     // .join("rect")
     // .attr("x", (d, i) => x(d.date) - 20)
@@ -180,8 +183,37 @@ export class StackedComponent {
     //   .style("fill", "steelblue");
   }
 
-  adjustYLabelCoords() {
+  smartLabelForceLayout(textEl, lineEl, textData) {
+    function onTick() {
+      textEl.attr("x", (d, i) => d.x)
+        .attr("y", d => d.y);
 
+      lineEl.attr("x1", (d, i) => d.x)
+        .attr("y1", d => d.y);
+    }
+
+    var sim = d3.forceSimulation(textData)
+      //.force('charge', d3.forceManyBody().strength(1))
+      .force('x', d3.forceX().x(function (d) {
+        return d.x
+      }))
+      .force('collision', d3.forceCollide().radius(function (d) {
+        return 7;
+      }))
+      .on('tick', onTick);
+
+    // slowSim();
+    // var i = 0;
+    // function slowSim() {
+    //   i++;
+    //   if (i > 1000) return;
+    //   console.log(i);
+    //   setTimeout(() => {
+    //     sim.tick();
+    //     onTick();
+    //     slowSim();
+    //   }, 300);
+    //}
   }
 
   showChart() {
