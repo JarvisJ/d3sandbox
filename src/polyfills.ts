363a8f82e5587d8e9a6ad396c3bc1f2d77b2638a
jjarvis <jjarvis@promodel.com>
2020-04-24 16:03:38 -0400

    revertin polyfill changes, broke codesandbox site

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,15 +42,7 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es';
-import 'core-js/es/reflect';
-import 'core-js/es/array';
-import 'classlist-polyfill';
-
-//require('zone.js/dist/zone.js');
- 
-import 'core-js/features/reflect';
-//import "core-js/es7/reflect";
+import "core-js/es7/reflect";
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
